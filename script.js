640687d448a8859a8376c90df309ddc14b7b8049
Mayank Rawat <rawatmayankofc25@gmail.com>
2025-11-17 18:26:30 +0530

    Add playGame and finalResult functionality
    Adding playGame and finalResult functions to complete the game and show the final outcome of the game.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
 
 function getComputerChoice() {
@@ -22,32 +23,53 @@ function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     
     if (computerChoice === humanChoice) {
-       return(`Round tie! Play again...`)
+       return(`Round tie! Play continue...`)
     } else if (computerChoice === "paper" && humanChoice === "rock") {
-        computerScore++;
+        computerScore = ++computerScore;
         return (`You lose! ${computerChoice} beats ${humanChoice}`);
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
-        computerScore++;
+        computerScore = ++computerScore;
         return (`You lose! ${computerChoice} beats ${humanChoice}`);
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
-        computerScore++;
+        computerScore = ++computerScore;
         return (`You lose! ${computerChoice} beats ${humanChoice}`);
     } else {
-        humanScore++;
-        return (`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore = ++humanScore;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 function playGame() {
+    round++;
 
-    console.log(playRound(humanSelection, computerSelection));
-    console.log(computerScore);
-    console.log(humanScore);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(`Round: ${round}`);
+    let result = playRound(humanSelection, computerSelection);
+    console.log(`Computer Score: ${computerScore}`);
+    console.log(`Player Score: ${humanScore}`);
+
+    return result;
 }
 
 
-playGame();
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
 
+
+function finalResult() {
+    if (computerScore === humanScore) {
+        console.log("Final Result: Match tie. Play again");
+    } else if (computerScore > humanScore) {
+        console.log("Final Result: Computer win the match");
+    } else {
+        console.log("Final Result: Player Win the match");
+    }
+}
+
+console.log(finalResult());
